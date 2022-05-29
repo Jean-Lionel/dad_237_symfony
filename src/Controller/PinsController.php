7b4f0e43bfd42442f33ffdf82b6c1908f6a94397
Jean-Lionel <nijeanlionel@gmail.com>
2022-05-29 11:10:18 +0200

    create form for saveing data

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -2,19 +2,52 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Pin;
+use App\Repository\PinRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PinsController extends AbstractController
 {
+    
     /**
-     * @Route("/pins", name="app_pins")
+     * @Route("/", name="app_pins")
      */
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
+        $repo = $em->getRepository(Pin::class);
+
+        $pins = $repo->findAll();
+
         return $this->render('pins/index.html.twig', [
-            'controller_name' => 'PinsController',
+            'name' => 'Jean Lionel Le hacker',
+            'pins' => $pins,
         ]);
+    }
+
+    /**
+     * @Route("/pins/create", methods={"GET", "POST"})
+     */
+
+    public function create(Request $request, EntityManagerInterface $em)
+    {
+        if ($request->isMethod('POST')) {
+            // code...
+           $data = $request->request->all();
+
+           $pin = new Pin();
+
+           $pin->setTitle($data['title']);
+           $pin->setDescription($data['description']);
+           $repo = $em->getRepository(Pin::class);
+           $repo->add($pin, true);
+
+           return $this->redirect('/');
+
+        }
+        return $this->render('pins/create.html.twig');
     }
 }
